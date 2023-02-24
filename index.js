43bcdf8a936ce7838ed3f1235a47d36822eaea4a
Andrei Lopatko <andrei.lopatko@together.com>
2023-02-24 13:42:01 +0200

    Add API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // import { express } from "express";
 const express = require('express');
 const path = require("path");
+const fs = require("fs");
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3007;
@@ -24,13 +25,27 @@ app.get('/admin', (req, res) => {
   });
 })
 
-// app.get('/api/getAll', (req, res) => {
-//
-// })
-//
-// app.post('/api/create', (req, res) => {
-//
-// })
+app.get('/api/getAll', (req, res) => {
+  fs.readFile(path.join(__dirname, 'database'), 'utf8',(err, json) => {
+    const data = JSON.parse(json);
+    res.json(data);
+  });
+})
+
+app.post('/api/create', async (req, res) => {
+  const { body } = req;
+  const newItem = {
+    id: Date.now(),
+    text: body.text,
+    title: body.title,
+  };
+  await fs.readFile(path.join(__dirname, 'database'), 'utf8',async (err, json) => {
+    const data = JSON.parse(json);
+    data.push(newItem);
+    await fs.writeFile(path.join(__dirname, 'database'), JSON.stringify(data), () => {});
+    res.send("Ok");
+  });
+})
 
 app.get('/store', (req, res) => {
   res.sendFile("store/store.html", {
