80f95dc2c2f42553076c8bfe1107fcbfdb28c5bf
Andrei Lopatko <andrei.lopatko@together.com>
2023-02-24 17:06:40 +0200

    Groomed

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3007;
 
+const SERVER_LAG = 1000;
+
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.redirect("/store");
-})
+});
 
 app.get('/admin', (req, res) => {
   res.sendFile("admin/admin.html", {
@@ -23,14 +25,28 @@ app.get('/admin', (req, res) => {
       'x-sent': true
     },
   });
-})
+});
+
+
+app.get('/store', (req, res) => {
+  res.sendFile("store/store.html", {
+    root: path.join(__dirname, 'public'),
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    },
+  });
+});
 
 app.get('/api/getAll', (req, res) => {
   fs.readFile(path.join(__dirname, 'database'), 'utf8',(err, json) => {
     const data = JSON.parse(json);
-    res.json(data);
+    setTimeout(() => {
+      res.json(data);
+    }, SERVER_LAG)
   });
-})
+});
 
 app.post('/api/create', async (req, res) => {
   const { body } = req;
@@ -43,23 +59,14 @@ app.post('/api/create', async (req, res) => {
     const data = JSON.parse(json);
     data.push(newItem);
     await fs.writeFile(path.join(__dirname, 'database'), JSON.stringify(data), () => {});
-    res.send("Ok");
+    setTimeout(() => {
+      res.send("Ok");
+    }, SERVER_LAG);
   });
-})
-
-app.get('/store', (req, res) => {
-  res.sendFile("store/store.html", {
-    root: path.join(__dirname, 'public'),
-    dotfiles: 'deny',
-    headers: {
-      'x-timestamp': Date.now(),
-      'x-sent': true
-    },
-  });
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
 
 
